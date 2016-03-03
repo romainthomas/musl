e0ea4152721e71f9b1c0239f614f2d4675a4f8c6
Romain THOMAS <romain.thomas999@gmail.com>
2016-03-03 13:11:25 +0100

    enable TLS

--- src/env/__libc_start_main.c
+++ src/env/__libc_start_main.c
@@ -35,7 +35,7 @@ void __init_libc(char **envp, char *pn)
 		for (i=0; pn[i]; i++) if (pn[i]=='/') __progname = pn+i+1;
 	}
 
-	//__init_tls(aux);
+	__init_tls(aux);
 	__init_ssp((void *)aux[AT_RANDOM]);
 
 	if (aux[AT_UID]==aux[AT_EUID] && aux[AT_GID]==aux[AT_EGID]
